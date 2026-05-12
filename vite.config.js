72bfe75e7125e3c6ae4de2ae09d271705c768331
rodyfeedsbmore <rjointermain@gmail.com>
2026-05-12 13:51:11 -0700

    had to fix the base path in vite.config.js and update the deploy script in package.json to deploy to the gh-pages branch instead of the main branch. Also removed the unused DonorCarousel component from App.jsx and updated the image source in donors.jsx to point to the correct location of the QR code image.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/rodyfeedsbmore/',   // <- your repo name with leading & trailing slash
   plugins: [react()],
-  base: '/rodyfeedsbmore/'
 })
